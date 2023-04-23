71544d485d616661ef5b760458c5444efad59917
Bignose <bignos@gmail.com>
2023-04-23 17:40:55 +0200

    First commit on github

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "tokyonight-bg": "#24283b", 
+        "tokyonight-header": "#1f2335",
+        "tokyonight-footer": "#3b4261",
+        "tokyonight-title": "#ff9e64",
+        "tokyonight-reference": "#5eaba4",
+        "tokyonight-highlight": "#35dacb",
+        "tokyonight-string": "#9ece6a",
+        "tokyonight-section-title": "#7aa2f7",
+        "tokyonight-text": "#bfc9f3",
+      },
+    },
   },
   plugins: [],
 }
-
